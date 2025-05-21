30f0d1b5b3b87ccacae68d672c8e1eb7c4fff847
nagarkotiArun420 <nagarkotiarun420@gmail.com>
2025-05-21 19:58:05 +0545

    Update TermsController.java

--- main/java/com/FleetX/controller/TermsController.java
+++ main/java/com/FleetX/controller/TermsController.java
@@ -9,26 +9,35 @@ import java.io.IOException;
 
 /**
  * Servlet implementation class TermsController
+ * This controller handles requests to the Terms page
+ * Maps to the URL: /Terms
  */
 @WebServlet("/Terms")
 public class TermsController extends HttpServlet {
+	// Unique identifier for serialization
 	private static final long serialVersionUID = 1L;
-       
+             
     /**
-     * @see HttpServlet#HttpServlet()
+     * Default constructor for TermsController
+     * Calls the parent HttpServlet constructor
      */
     public TermsController() {
         super();
         // TODO Auto-generated constructor stub
     }
-
+	
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Handles HTTP GET requests for the Terms page
+	 * Forwards the request to the Terms.jsp view
+	 * 
+	 * @param request The HTTP request object
+	 * @param response The HTTP response object
+	 * @throws ServletException If a servlet-specific error occurs
+	 * @throws IOException If an I/O error occurs
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+		// Forward the request to the Terms JSP page for rendering
 		request.getRequestDispatcher("WEB-INF/Pages/Terms.jsp").forward(request, response);
 	}
-
-
 }
